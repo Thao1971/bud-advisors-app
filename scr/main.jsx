75bcd01a7bf6db8c0ccc8cc06541a55c743c738a
Thao1971 <158329667+Thao1971@users.noreply.github.com>
2026-02-19 17:26:47 +0100

    Add files via upload

--- scr/main.jsx
+++ scr/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Hemos cambiado './App.jsx' por '../App.jsx' para que suba un nivel y lo encuentre en la raíz
+import App from '../App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
